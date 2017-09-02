ce3714ebc3f210d90e9da9f25bc657659fc7b411
snoozesecurity <snoozesec@gmail.com>
2017-09-01 17:45:56 -0700

    Simple URL/URI defanger

---
+++ defang.py
@@ -0,0 +1,27 @@
+# Simple and possibly redundant URL/URI defanger.  
+# Can take a file as an argument, otherwise treats all 
+# text to the left of first whitespace as a string argument.
+# youtube.com/snoozesecurity
+
+import re
+import sys
+import os.path
+
+defanged = []
+
+def defang(url):
+        newurl =  re.sub('[.]', '[.]', url)
+        newurl = newurl.replace("http", "hxxp", 1)
+	return newurl
+
+if os.path.isfile(sys.argv[1]) == True:
+	with open(sys.argv[1]) as f:
+   		for line in f:
+       			defanged.append(defang(line))
+	for p in defanged: print p.rstrip("\n\r")
+
+elif "." in sys.argv[1] or "http" in sys.argv[1]: 
+	print 'Defanged as string:', defang(sys.argv[1])
+
+else:
+	print 'Not defanged:', defang(sys.argv[1])
